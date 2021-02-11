650cdbf4be089614c283f0298ea00f53e93bd01a
Owonubi Job Sunday <jobowonubi@gmail.com>
2021-02-11 21:31:48 +0100

    Create Carts Table

--- database/migrations/2021_02_11_200915_create_carts_table.php
+++ database/migrations/2021_02_11_200915_create_carts_table.php
@@ -15,6 +15,9 @@ class CreateCartsTable extends Migration
     {
         Schema::create('carts', function (Blueprint $table) {
             $table->id();
+            $table->integer('user_id')->unsigned();
+            $table->integer('product_id')->unsigned();
+            $table->integer('quantity');
             $table->timestamps();
         });
     }
