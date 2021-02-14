c58abd470ae5dece4f2761219859d3fb83963b53
Owonubi Job Sunday <jobowonubi@gmail.com>
2021-02-14 15:48:56 +0100

    Fixed Toggle

---
+++ resources/views/admin/partials/scripts.blade.php
@@ -0,0 +1,14 @@
+<script src="https://code.jquery.com/jquery-3.5.1.min.js" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.bundle.min.js" crossorigin="anonymous">
+</script>
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.8.0/Chart.min.js" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/startbootstrap-sb-admin@6.0.2/dist/assets/demo/chart-area-demo.js">
+</script>
+<script src="https://cdn.jsdelivr.net/npm/startbootstrap-sb-admin@6.0.2/dist/assets/demo/chart-bar-demo.js">
+</script>
+<script src="https://cdn.datatables.net/1.10.20/js/jquery.dataTables.min.js" crossorigin="anonymous"></script>
+<script src="https://cdn.datatables.net/1.10.20/js/dataTables.bootstrap4.min.js" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/startbootstrap-sb-admin@6.0.2/dist/assets/demo/datatables-demo.js">
+</script>
+<script src="https://cdn.jsdelivr.net/npm/startbootstrap-sb-admin@6.0.2/dist/js/scripts.js"></script>
