2c22cb7434f0fab2895a98ff17e7ad41e3fa9cab
Owonubi Job Sunday <jobowonubi@gmail.com>
2021-02-15 09:30:25 +0100

    Removed unused code

--- resources/views/customer/search.blade.php
+++ resources/views/customer/search.blade.php
@@ -8,8 +8,6 @@
         <div class='row'>
             @if (count($products))
                 @foreach ($products as $product)
-                    {{-- $highlighted = preg_filter('/' . preg_quote($_POST['keyword'], '/') . '/i', '<b>$0</b>', $row['name']) --}}
-
                     <div class='col-sm-4'>
                         <div class='card mb-3'>
                             <div class='box-body p-2'>
@@ -33,65 +31,3 @@
     </div>
 
 @endsection
-
-
-{{-- <?php
-$conn = $pdo->open();
-
-$stmt = $conn->prepare('SELECT COUNT(*) AS numrows FROM products WHERE name LIKE :keyword');
-$stmt->execute(['keyword' => '%' . $_POST['keyword'] . '%']);
-$row = $stmt->fetch();
-if ($row['numrows'] < 1) {
-    echo '<h1 class="page-header">No results found for <i>' . $_POST['keyword'] . '</i></h1>';
-} else {
-    echo '<h1 class="page-header">Search results for <i>' . $_POST['keyword'] . '</i></h1>';
-    try {
-        $inc = 3;
-        $stmt = $conn->prepare('SELECT * FROM products WHERE name LIKE :keyword');
-        $stmt->execute(['keyword' => '%' . $_POST['keyword'] . '%']);
-
-        foreach ($stmt as $row) {
-            $highlighted = preg_filter('/' . preg_quote($_POST['keyword'], '/') . '/i', '<b>$0</b>', $row['name']);
-            $image = !empty($row['photo']) ? 'images/' . $row['photo'] : 'images/noimage.jpg';
-            $inc = $inc == 3 ? 1 : $inc + 1;
-            if ($inc == 1) {
-                echo "<div class='row'>";
-            }
-            echo "
-	       							<div class='col-sm-4'>
-	       								<div class='box box-solid'>
-		       								<div class='box-body prod-body'>
-		       									<img src='" .
-                $image .
-                "' width='100%' height='230px' class='thumbnail'>
-		       									<h5><a href='product.php?product=" .
-                $row['slug'] .
-                "'>" .
-                $highlighted .
-                "</a></h5>
-		       								</div>
-		       								<div class='box-footer'>
-		       									<b>&#36; " .
-                number_format($row['price'], 2) .
-                "</b>
-		       								</div>
-	       								</div>
-	       							</div>
-	       						";
-            if ($inc == 3) {
-                echo '</div>';
-            }
-        }
-        if ($inc == 1) {
-            echo "<div class='col-sm-4'></div><div class='col-sm-4'></div></div>";
-        }
-        if ($inc == 2) {
-            echo "<div class='col-sm-4'></div></div>";
-        }
-    } catch (PDOException $e) {
-        echo 'There is some problem in connection: ' . $e->getMessage();
-    }
-}
-
-$pdo->close();
-?> --}}
